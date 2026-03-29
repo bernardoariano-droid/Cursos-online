dc44f144135ca3544a502c7ef6cd12c841bb6eef
bernardoariano-droid <bernardoariano@gmail.com>
2026-03-29 07:48:15 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.angolearn.app',
+  appName: 'AngoLearn',
+  webDir: 'dist'
+};
+
+export default config;
